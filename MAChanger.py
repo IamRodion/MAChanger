4c2671a723e35559851f91e39640a8fa3f035b67
__Rodion__ <pablotapias98@gmail.com>
2021-10-27 23:39:53 +0000

    Update MAChanger.py
    Agregada detección de interfaces de red automática

--- MAChanger.py
+++ MAChanger.py
@@ -1,4 +1,4 @@
-import subprocess, os, random
+import subprocess, os, random, netifaces
 
 INFO = "[i] Creado por __Rodion__. (github.com/RodionButEncapsulated)\n"
 
@@ -20,12 +20,24 @@ Yb, `88'  `88'  `88               d8"     `Y8 IP'`Yb
                                                                                       `8, ,8I                     
                                                                                        `Y8P"                      """
 
-def clean_screen():
-    if os.name == "posix":
-        os.system("clear")
-    else:
-        os.system("cls")
+menu1 = "\t INTERFAZ A EJECUTAR EL CAMBIO\n"
+menu2 = "\t NUEVA DIRECCIÓN MAC\n"
 
+def clean_screen(): # Función para limpiar pantalla.
+    os.system("clear")
+
+def check_interfaces(): # Revisa las interfaces de red disponibles en el equipo.
+    interfaces = netifaces.interfaces()
+    return interfaces
+
+def select_interface(): # Muestra las interfaces de red en orden.
+    print(menu1)
+    interfaces = check_interfaces() # Obtener las interfaces disponibles.
+    for interface in interfaces:
+        print(f"[{interfaces.index(interface)}] {interface}") # Mostrar las interfaces disponibles.
+    choice = int(input("\n[?] Elija una opción: "))
+    
+    return interfaces[choice] # Devuelve la interfaz de red seleccionada.
 
 def random_mac(): # Genera una dirección MAC aleatoria.
     chars = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]
@@ -42,36 +54,47 @@ def random_mac(): # Genera una dirección MAC aleatoria.
 
     return mac
 
+def select_mac(): # Selecciona una MAC, aleatoria con la función random_mac() o manualmente.
+    print(menu2)
+    print("[0] MAC Random\n[1] MAC Manual")
+    choice = input("\n[?] Elija una opción: ")
+
+    if choice == "0":
+        mac = random_mac() # Genera una MAC aleatoria.
+        return mac
+    elif choice == "1":
+        mac = input("[?] Indique la MAC que desea: ") # Pide una MAC al usuario.
+        return mac
+    else:
+        print("[!] La opción indicada no es correcta, MAC aleatoria por default seleccionada.")
+        mac = random_mac() # Genera una MAC aleatoria.
+        return mac
+
+
+
 def change_mac(mac, interface): # Toma como argumento una MAC y cambia la actual por esa.
     subprocess.call(f"sudo ifconfig {interface} down", shell=True)
     subprocess.call(f"sudo ifconfig {interface} hw ether {mac}", shell=True)
     subprocess.call(f"sudo ifconfig {interface} up", shell=True)
-    print(f"\n--------------- ( ͡❛ ͜ʖ ͡❛) MAC cambiada correctamente a {mac} ( ͡❛ ͜ʖ ͡❛) ---------------\n")
+    print(f"\n--------------- ( ͡❛ ͜ʖ ͡❛) MAC de {interface} cambiada correctamente a {mac} ( ͡❛ ͜ʖ ͡❛) ---------------\n")
     subprocess.call(f"sudo ifconfig {interface}", shell=True) # Muestra la información de la interfaz seleccionada.
 
 def main(): # Función principal del código.
     clean_screen() # Limpiar pantalla al inicio del código.
 
     print(LOGO)
-    print(INFO)    
+    print(INFO)
+    interface = select_interface() # Obtiene la interfaz de red a trabajar.
 
-    interface = input("[?] Ingrese la interfaz a hacer el cambio. (Enter para wlan0): ")
-    mac = input("[?] Ingrese la nueva dirección MAC (Enter para una aleatoria): ")
+    clean_screen() # Limpiar pantalla luego de obtener la interfaz a trabajar.
 
-    if mac == "":
-        mac = random_mac()
-        if interface == "":
-            interface = "wlan0"
-            change_mac(mac, interface) #Cambia la dirección MAC
-        else:
-            change_mac(mac, interface) #Cambia la dirección MAC
-    else:
-        if interface == "":
-            interface = "wlan0"
-            change_mac(mac, interface) #Cambia la dirección MAC
-        else:
-            change_mac(mac, interface) #Cambia la dirección MAC
+    print(LOGO)
+    print(INFO)
+    print(f"Interfaz Seleccionada: {interface}\n")
+    mac = select_mac() # Obtiene la mac que se establecerá.
+    
 
+    change_mac(mac, interface) #Cambia la dirección MAC
 
 
 if __name__ == "__main__": # Inicializador.
